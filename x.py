e804f95c277986659e4612b115686771dc3e42e5
firbolg <levijdolan@gmail.com>
2021-06-01 15:37:11 -0500

    first commit- import csv and get non-nan sums

---
+++ x.py
@@ -0,0 +1,24 @@
+import csv
+import pandas as pd
+import os
+
+directory = r'C:\Users\levi\source\repos\Preprints_Qual\Preprints_Qual'
+
+for csv_file in os.scandir(directory):
+    if csv_file.path.endswith(".csv"):
+        df = pd.read_csv(csv_file)
+        for column in df:
+            code_category = df.loc[:, str(column)]
+            not_nan = pd.notna(code_category)
+            count = not_nan.values
+            print(str(column) + ": " + str(sum(count)))
+    else:
+        continue 
+
+
+
+
+#column = df.loc[:,'Author Updates']
+#not_nan = pd.notna(column)
+#count = not_nan.values
+#print(sum(count))
